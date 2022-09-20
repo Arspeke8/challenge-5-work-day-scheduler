f60f4af3508c2be0d21aa6162e8efc2caaae3a0f
Arspeke8 <110306643+Arspeke8@users.noreply.github.com>
2022-09-19 22:36:11 -0700

    changed to 24 hour clock

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 var timeDisplayEl = $("#time-display");
+var pastTime = $("#past");
+var presentTime = $("#present");
+var futureTime = $("#future");
 
 function displayTime() {
-  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  var rightNow = moment().format("MMM DD, YYYY [at] HH:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
 setInterval(displayTime, 1000);
